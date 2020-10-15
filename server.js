3d5e138ac0d0fcde66add2e95363dfdb4867be11
Ken Marchand <marchand_kenneth@smc.edu>
2020-10-15 10:07:23 -0700

    Update Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { animals } = require('./data/animals');
 
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -50,5 +51,5 @@ app.get('/api/animals',(req,res)=>{
     res.json(results)
 });
 app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
   });
