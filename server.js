f48ef7b7e53cfe094e8b907394758f2482775ef4
Ken Marchand <marchand_kenneth@smc.edu>
2020-10-15 13:19:12 -0700

    Updated Code with second get

--- server.js
+++ server.js
@@ -40,7 +40,10 @@ if (query.name) {
 // return the filtered results:
 return filteredResults;
 }
-
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -50,8 +53,13 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
   });
 
-app.get('/api/animals', (req, res) => {
-    res.json(animals);
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+      } else {
+        res.send(404);
+      }
   });
 
 
